2f68df7c520597f8886dfbd11f0a6f1acdd67683
Ayomitanomotayo <ayomiotanomotayo@gmail.com>
2023-02-21 02:15:20 -0800

    lower case alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - A c program to print lower case alphabet a - z
+ * Return: Always (success)
+ */
+
+int main(void)
+
+{
+	char C;
+
+	for (C = 'a'; C  <= 'z'; C++)
+	putchar(C);
+	putchar('\n');
+	return  (0);
+
+}
