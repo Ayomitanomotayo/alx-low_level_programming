13ef77d9c0e88b5a8f2740f8a5ecac807aeb7faa
Ayomitanomotayo <ayomiotanomotayo@gmail.com>
2023-02-24 09:09:18 -0800

    isupper

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - writes the charcter c to stdout
+ * @c: the character to point
+ *
+ * Return: on success 1.
+ * On error, -1 is returned, and error is set appropriately
+ */
+int_put(char c)
+{
+return; (write(1, &c, 1))
+}
