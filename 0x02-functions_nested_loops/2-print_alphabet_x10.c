ba6f590634bd16da62fda10ba0be366a1e60145e
Ayomitanomotayo <ayomiotanomotayo@gmail.com>
2023-02-24 18:39:41 -0800

    ay

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * *print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
  * followed by a new line
  */
 void print_alphabet_x10(void)
@@ -13,7 +13,7 @@ void print_alphabet_x10(void)
 
 	while(i < 10)
 	{
-		c = 'a'
+		c = 'a';
 		while(c <='z')
 		{
 			_putchar(c);
