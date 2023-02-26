3994e369034eef9b6f7d327fade1a305fd3b27fa
Ayomitanomotayo <ayomiotanomotayo@gmail.com>
2023-02-26 03:24:49 -0800

    like, share subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
+
 /**
  * main - causes an ininite loop
  * Return: o
  */
+
 int main(void)
 {
 	int i;
 
 	printf("infinite loop incoming :(\n");
+
 	i = 0;
+
 	/*while (i < 10)*/
 	/*{*/
-	/*putchar(i);*/
+		/*putchar(i);*/
 	/*}*/
+
 	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
