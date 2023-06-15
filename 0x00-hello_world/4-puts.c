5785f6309d8d98852c68c79cecfbbddf46b068c3
Ayomiotanomotayo <ayomiotaomotay@gmail.com>
2023-06-15 16:30:40 -0700

    Printing a string with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
-int main()
+
+/**
+ * main - print programming is like building a multilingual puzzle
+ *
+ * Return: always succes
+ */
+
+int main(void)
 {
-	char* sname = "Programming is like building a multilingual puzzle";
-	puts (sname);
+	char *sname = "Programming is like building a multilingual puzzle";
+
+	puts(sname);
 	return (0);
 }
