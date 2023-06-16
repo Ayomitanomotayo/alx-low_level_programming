d66c13937e06eb55db2546bf1fc88dc52a8cd6c2
Ayomiotanomotayo <ayomiotaomotay@gmail.com>
2023-06-15 18:17:46 -0700

    printing to standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * main - printing strings to standard error
+ *
+ * Return: 1
+ */
+int main() {
+    const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(2, message, sizeof(message) - 1);
+    return 1;
+}
