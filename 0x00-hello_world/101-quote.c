588c92702ca5eeca250594663427ab1dfe69643a
Ayomiotanomotayo <ayomiotaomotay@gmail.com>
2023-06-15 18:32:41 -0700

    Printing string to standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,11 +4,9 @@
  *
  * Return: 1
  */
-int main(void)
+int main()
 {
-	const char message[] = "and that piece of art
-		is useful\" - Dora Korpar, 2015-10-19\n";
-
-		write(2, message, sizeof(message) - 1);
-		return (1);
+	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, message, sizeof(message) - 1);
+	return(1);
 }
