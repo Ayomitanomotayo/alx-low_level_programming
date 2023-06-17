fade3c7e6046007d99e563bb83d9bd73db6c681b
Ayomiotanomotayo <ayomiotaomotay@gmail.com>
2023-06-17 04:46:54 -0700

    Printing digit from 0 - 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - printing digit 0-9
+ *
+ * Return: Always success
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		putchar('0' + num);
+			num++;
+	}
+	putchar('\n');
+	return (0);
+}
