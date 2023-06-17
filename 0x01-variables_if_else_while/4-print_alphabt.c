aeafe5caa0e95ae8eda839811ec4d10a61f2053a
Ayomiotanomotayo <ayomiotaomotay@gmail.com>
2023-06-17 04:14:47 -0700

    Printing lowercase number excepting q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,10 +9,14 @@ int main(void)
 	char ch = 'a';
 
 		while (ch <= 'z')
-		if (ch != 'q' && ch != 'e')
 		{
-		putchar(ch);
-		ch++;
+			if (ch != 'q' && ch != 'e')
+			{
+			putchar(ch);
+			}
+			{
+			ch++;
+			}
 		}
 		putchar('\n');
 		return (0);
