f5c1f495ce2a04ee32a46c14dc8279a6c28f792e
Ayomiotanomotayo <ayomiotaomotay@gmail.com>
2023-06-20 16:53:46 -0700

    printing absolute value if an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - computing absolute value of an integer
+ *
+ * Return: Alway -num or num
+ */
+int _abs(int num)
+{
+	if (num < 0)
+	{
+		return (-num);
+	}
+	else
+	{
+		return (num);
+	}
+}
