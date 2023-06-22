3b9a4a6acf43f357510cc079e60471d72c59180e
Ayomiotanomotayo <ayomiotaomotay@gmail.com>
2023-06-22 15:25:25 -0700

    Checking for uppercase letter

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isupper - checking for upper case letters
+ *@c: letter to check for
+ *
+ * Return: Always succes
+ */
+
+int _isupper(int c)
+{
+	if (c == 'A')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
