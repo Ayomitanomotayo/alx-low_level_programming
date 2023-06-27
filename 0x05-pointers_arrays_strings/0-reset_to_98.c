151677bdffde7d4f8a9d5f902df3eb946c64de37
Ayomiotanomotayo <ayomiotaomotay@gmail.com>
2023-06-27 06:11:41 -0700

    Updating the value of a varible using pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - function for reset
+ *@n: Pointer to value to be dereferenced
+ *
+ * Return: Always success
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
