73c81ec97a5b4b2b2d1e9aee4d86513abe4d894e
Ayomiotanomotayo <ayomiotaomotay@gmail.com>
2023-06-27 06:18:24 -0700

    Swapping two variables using pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - Swapping two variables using pointer
+ * @a: The first variable
+ * @b: The second variable
+ *
+ * Return: Always success
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
