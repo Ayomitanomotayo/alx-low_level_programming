66bbc2437735ff71577f1b4dd004f68acf5a4605
Ayomiotanomotayo <ayomiotaomotay@gmail.com>
2023-07-11 11:27:10 -0700

    Concatenating two string and allocating memory to destination string

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,6 +13,10 @@ char *str_concat(char *s1, char *s2)
 	char *dest;
 	size_t size = strlen(s1) + strlen(s2) + 1;
 
+	if (s1 == NULL)
+	s1 = "";
+	if (s2 == NULL)
+	s2 = "";
 	dest = (char *)malloc(sizeof(char) * size);
 	if (dest == NULL)
 		return (NULL);
