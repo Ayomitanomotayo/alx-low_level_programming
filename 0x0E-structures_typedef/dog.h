8b064632fc8fb36fb44d7cd29a3a8bb97a06b6e0
Ayomiotanomotayo <ayomiotaomotay@gmail.com>
2023-07-18 11:46:58 -0700

    A function that prints struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- * struct dog - A data type used to collect dog Id's
+ * typedef struct dog dog_t- A data type used to collect dog Id's
  *@name: The name of the dog
  *@age: The age of the dog
  *@owner: The details of the dog owner
@@ -14,5 +14,11 @@ struct dog
 	float age;
 	char *owner;
 };
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif
