029404c97338372adb187768d7ebef3e103511f8
Ayomiotanomotayo <ayomiotaomotay@gmail.com>
2023-07-18 07:26:39 -0700

    Definning a new type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - A data type used to collect dog Id's
+ *@name: The name of the dog
+ *@age: The age of the dog
+ *@owner: The details of the dog owner
+ *
+ * Return: Alway success
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
