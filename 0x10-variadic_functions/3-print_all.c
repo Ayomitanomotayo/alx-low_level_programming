4e3b9a6ada318279cb40c76443c3939657b101cc
Ayomiotanomotayo <ayomiotaomotay@gmail.com>
2023-07-20 18:45:21 -0700

    A variadic function that prints all type

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,43 +1,37 @@
 #include <stdio.h>
 #include <stdarg.h>
+/**
+ * print_all - A variadic function that prints all types
+ *@*: pointer to all types
+ *@format: The first argument
+ *@...: Elipse variadic arguments
+ * Return: Always success
+ */
 void print_all(const char * const format, ...)
 {
 	const char *first_format = format;
-	char c;
-	int i;
-	float f;
-	char *s;
 	va_list args;
 
 	va_start(args, format);
+	while (*format)
+	{
+	if (*format == 'c')
+	{
+	putchar(va_arg(args, int));
+	}
+	else if (*format == 'i')
+	{
+	printf("%d", va_arg(args, int));
+	}
+	else if (*format == 'f')
+	{
+	printf("%f", va_arg(args, double));
+	}
+	else if (*format == 's')
+	{
+	const char *str = va_arg(args, const char*);
 
-	while (*first_format)
-	{
-	switch (*first_format)
-	{
-	case 'c':
-	c = va_arg(args, int);
-	printf("%c", c);
-	break;
-	case 'i':
-	i = va_arg(args, int);
-	printf("%d", i);
-	break;
-	case 'f':
-	f = va_arg(args, double);
-	printf("%f", f);
-	break;
-	case 's':
-	s = va_arg(args, char *);
-	if (s == NULL)
-	{
-	printf("(nil)");
-	}
-	else
-	{
-	printf("%s", s);
-	}
-	break;
+	printf("%s", str ? str : "(nil)");
 	}
 	first_format++;
 	if (*first_format)
